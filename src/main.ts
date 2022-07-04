bb889ae80405032d5288be312a520302796c0034
pirak <82913078+pirak@users.noreply.github.com>
2022-07-04 20:25:55 +0200

    prepare for validation via backend

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { CustomScriptManifest, DefaultParametersConfig, Firebot, RunRequest } from 'firebot-custom-scripts-types';
 import { buildGambleEffect } from './gamble-effect';
+import { FrontendCommunicator } from 'firebot-custom-scripts-types/types/modules/frontend-communicator';
 
 const scriptVersion = '1.0.1';
+
+export enum EventNames {
+    VALIDATE_INPUT = 'pirak-custom-gambling-validate-input',
+}
 
 export interface ScriptParams extends Record<string, unknown> {}
 
@@ -21,10 +26,22 @@ export class GamblingScript implements Firebot.CustomScript<ScriptParams> {
         };
     }
 
+    private static setupFrontendListeners(frontendCommunicator: FrontendCommunicator): void {
+        console.log('Registering event: ' + EventNames.VALIDATE_INPUT);
+        frontendCommunicator.onAsync<any, Array<string>>(EventNames.VALIDATE_INPUT, validateRanges);
+    }
+
     public run(runRequest: RunRequest<ScriptParams>): void {
         runRequest.modules.logger.info('Registering Gambling Effect...');
         runRequest.modules.effectManager.registerEffect(buildGambleEffect(runRequest));
+        GamblingScript.setupFrontendListeners(runRequest.modules.frontendCommunicator);
     }
+}
+
+export async function validateRanges(ranges: Array<Range>): Promise<string[]> {
+    console.log(ranges);
+    // ToDo: actual validation when the event is actually triggered
+    return Promise.resolve(['some err from backend']);
 }
 
 export default new GamblingScript();
